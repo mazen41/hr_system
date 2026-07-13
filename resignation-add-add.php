1759e11a547cb9fdd1ea3f3d158e5bffe83e41e1
Mazen Hossny <mazehossny@gmail.com>
2026-07-13 04:48:23 +0300

    message

--- resignation-add-add.php
+++ resignation-add-add.php
@@ -143,24 +143,6 @@ if(isset($_GET['id'])){
     border-color: #b8daff;
     color: #0c5460;
   }
-  /* Ensure preloading is styled correctly if you use it */
-  #preloading {
-    display: none; /* Hide by default, shown by JS */
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(255, 255, 255, 0.7);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9999;
-  }
-  #preloading img {
-    width: 50px; /* Adjust size of your loading gif */
-    height: 50px;
-  }
 </style>
 
     <div class="content-header page-nav">
